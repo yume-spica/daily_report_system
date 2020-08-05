6b85797cd820918ab7ea6754d9bc996eed8cc592
iroha <yuuteiyuu@gmail.com>
2020-08-05 12:51:06 +0900

    end

--- src/controllers/reports/ReportsUpdateServlet.java
+++ src/controllers/reports/ReportsUpdateServlet.java
@@ -44,6 +44,7 @@ public class ReportsUpdateServlet extends HttpServlet {
             r.setReport_date(Date.valueOf(request.getParameter("report_date")));
             r.setTitle(request.getParameter("title"));
             r.setContent(request.getParameter("content"));
+            r.setTask(request.getParameter("task"));
             r.setUpdated_at(new Timestamp(System.currentTimeMillis()));
 
             List<String> errors = ReportValidator.validate(r);
